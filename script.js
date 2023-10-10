a352a972364c6138bdfe31cd00c3a528b27c9afe
Zeynep Aydin <78844470+z1almagul@users.noreply.github.com>
2023-10-10 23:41:26 +0200

    update

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ window.addEventListener('load',function(){
             });
             
             window.addEventListener('touchmove', e => {
-                this.ui.score = 'hi';
+                this.game.ui.score = 'hi';
             //    for(let i = 0; i < e.touches.length; i++){
             //        var touch = e.touches.item[i];
             //        this.game.player.speedY= touch.pageY - this.game.player.height;
